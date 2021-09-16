55c127c34673cf9e48e050d0892c349db58827b6
Nishal Gurung <nishal.gurung4@outlook.com>
2021-09-16 23:58:31 +0545

    fix formatter issue

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,14 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->notPath('vendor')
+    ->notPath('bootstrap')
+    ->notPath('storage')
+    ->in(__DIR__)
+    ->name('*.php')
+    ->notName('*.blade.php');
+
+
+return Nishal\styles($finder ,[
+    'array_indentation' => true
+]);
